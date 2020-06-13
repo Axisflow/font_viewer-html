33978599ab17a5839a1f4431a2fb7ef4dd0d45ca
王證傑 <new.computeruser.old@gmail.com>
2020-06-13 14:15:32 +0800

    ...

--- advertisement.js
+++ advertisement.js
@@ -1,4 +1,4 @@
-var adsContent = ['AD', '<iframe title=\"Advertisement\" width=\"450\" height=\"450\" src=\"http://github.com/Axialflow\"></iframe>'];
+var adsContent = ['AD', '<iframe title=\"Advertisement\" width=\"450\" height=\"450\" src=\"https://github.com/Axialflow\"></iframe>'];
 
 function showAD(linkto) {
     var content = '<div style=\"background-color: #151515;display: flex;align-items: center;justify-content: center;width: 100%;height: 100%;\">' + adsContent[1] + '<a href=\"' + linkto + '\" style=\"position: absolute;bottom: 10px;color: #e5e5e5;font-size: 30px;\">GO!</a></div>';
