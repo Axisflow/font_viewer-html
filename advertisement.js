bbe5e65a9d1e90c7ce785771909d7a91717fc2d4
王證傑 <new.computeruser.old@gmail.com>
2020-06-13 14:51:33 +0800

    ...

--- advertisement.js
+++ advertisement.js
@@ -1,6 +1,6 @@
-var adsContent = ['AD', '<iframe title=\"Advertisement\" width=\"450\" height=\"450\" src=\"https://Axialflow.github.io/"></iframe>'];
+var adsContent = ['AD', '<iframe title=\"Advertisement\" width=\"450\" height=\"450\" src=\"https://cysh-cc.github.io/"></iframe>'];
 
 function showAD(linkto) {
-    var content = '<div style=\"background-color: #151515;color: #e5e5e5;display: flex;align-items: center;justify-content: center;width: 100%;height: 100%;\">' + adsContent[1] + '<a href=\"' + linkto + '\" style=\"position: absolute;bottom: 10px;color: #e5e5e5;font-size: 30px;\">GO!</a></div>';
+    var content = '<div style=\"background-color: #151515;color: #e5e5e5;display: flex;align-items: center;justify-content: center;width: 100%;height: 100%;\"><p style=\"position: absolute;top: 12.5px;\">廣告</p>' + adsContent[1] + '<a href=\"' + linkto + '\" style=\"position: absolute;bottom: 25px;color: #e5e5e5;font-size: 30px;\">GO!</a></div>';
     document.write(content);
 }
